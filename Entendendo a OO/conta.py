b39da22778508568f4c06674b90bc57190c5f843
Alice Rodrigues <alicerds@id.uff.br>
2022-08-09 11:05:28 -0300

    2022/08/09

--- Entendendo a OO/conta.py
+++ Entendendo a OO/conta.py
@@ -2,5 +2,13 @@
 
 class ContaCorrente:
 
+    def __init__(self, numero, titular, saldo, limite = 1000.0):
+
+        print("Construindo objeto... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
     pass
 
